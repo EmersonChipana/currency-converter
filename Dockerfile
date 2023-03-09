b2722f3aec327e6c89aa4d1fe96a849af38399f6
Joel Rojas <jrojas@cirrus-it.net>
2023-03-09 14:00:19 -0400

    Agregando seguridad

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,37 @@
+#FROM eclipse-temurin:11
+#COPY target/*.jar app.jar
+#
+#ENV USERNAME "postgres"
+#ENV PASSWORD "mysecretpassword"
+#ENV URL "jdbc:postgresql://localhost:5432/software"
+#ENV API_KEY ""
+#
+#ENTRYPOINT ["java","-jar","/app.jar"]
+
+
+FROM --platform=linux/x86_64 eclipse-temurin:11.0.18_10-jdk-alpine as build
+WORKDIR /workspace/app
+
+COPY mvnw .
+COPY .mvn .mvn
+COPY pom.xml .
+COPY src src
+
+RUN ./mvnw install -DskipTests
+RUN mkdir -p target/dependency && (cd target/dependency; jar -xf ../*.jar)
+
+
 FROM eclipse-temurin:11
-COPY target/*.jar app.jar
+
+VOLUME /tmp
+ARG DEPENDENCY=/workspace/app/target/dependency
+COPY --from=build ${DEPENDENCY}/BOOT-INF/lib /app/lib
+COPY --from=build ${DEPENDENCY}/META-INF /app/META-INF
+COPY --from=build ${DEPENDENCY}/BOOT-INF/classes /app
 
 ENV USERNAME "postgres"
 ENV PASSWORD "mysecretpassword"
 ENV URL "jdbc:postgresql://localhost:5432/software"
+ENV API_KEY ""
 
-ENTRYPOINT ["java","-jar","/app.jar"]
+ENTRYPOINT ["java","-cp","app:app/lib/*","arquitectura.software.demo.DemoApplicationKt"]
--- src/main/kotlin/arquitectura/software/demo/api/CurrencyApi.kt
+++ src/main/kotlin/arquitectura/software/demo/api/CurrencyApi.kt
@@ -5,12 +5,15 @@ import arquitectura.software.demo.dto.ResponseServiceDto
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.security.access.prepost.PreAuthorize
+import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 import java.math.BigDecimal
 import java.math.BigInteger
+import java.security.Principal
 
 @RequestMapping("/api/currency")
 @RestController
@@ -27,6 +30,25 @@ class CurrencyApi @Autowired constructor(private val currencyBl: CurrencyBl) {
         LOGGER.info("Iniciando peticion para convertir divisas de $from a $to con un monto de $amount")
         val result = currencyBl.exchangeRate(to, from, amount)
         return result
+    }
+
+    @GetMapping("/user")
+    @PreAuthorize("hasAuthority('ROLE_USER')")
+    fun user(): String {
+        LOGGER.info("Iniciando servicio user")
+        return "ROLE_USER"
+    }
+
+    @GetMapping("/admin")
+    @PreAuthorize("hasAuthority('ROLE_ADMIN')")
+    fun admin(): String {
+        LOGGER.info("Iniciando servicio admin")
+        return "ROLE_ADMIN"
+    }
+
+    @GetMapping("/principal")
+    fun info(principal: Principal): String {
+        return principal.toString()
     }
 
 }
